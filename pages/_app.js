17480ae2f13378f1420a3817e6a4c393288a5c0f
Ashraf <ashrafdev365@gmail.com>
2023-01-01 20:38:33 -1200

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider, Hydrate } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 //initilize the Query
 import Layout from "../components/Layout";
+import { Analytics } from '@vercel/analytics/react';
 
 const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }) {
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }) {
         <Hydrate state={pageProps.dehydratedState}>
           <Layout>
             <Component {...pageProps} />
+            <Analytics />
             {/* <ReactQueryDevtools initialIsOpen={false} position="bottom-right" /> */}
           </Layout>
         </Hydrate>
